2fe4dbb65b32ee41c9aa3204fc5f45cfd6df6edf
angeluzunov10 <angel_d_u@abv.bg>
2024-10-15 19:51:42 +0300

    Added our condition for not including title in content, using clean() method.

--- forumApp/posts/forms.py
+++ forumApp/posts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from forumApp.posts.mixins import DisableFieldsMixin
 from forumApp.posts.models import Post
@@ -15,6 +16,23 @@ class PostForm(forms.ModelForm):
                 'max_length': f'The title is too long. Please keep it under {Post.TITLE_MAX_LENGTH} characters.'
             }
         }
+
+    def clean_author(self):
+        author = self.cleaned_data.get('author')
+
+        if author[0] != author[0].upper():
+            raise ValidationError('Author name must start with a capital letter!')
+
+        return author
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        title = cleaned_data.get('title')
+        content = cleaned_data.get('content')
+
+        if title and content and title in content:
+            raise ValidationError('The post title cannot be included in the post content!')
 
 
 class PostCreateForm(PostForm):
