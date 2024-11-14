052903514cb999f435867c76692f9595ec6d828a
angeluzunov10 <angel_d_u@abv.bg>
2024-11-14 17:25:07 +0200

    Registered user in admin panel and attached him to View-Only permissions group.

--- forumApp/accounts/admin.py
+++ forumApp/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
+
+UserModel = get_user_model()
+
+
+@admin.register(UserModel)
+class AdminUser(admin.ModelAdmin):
+    pass
+
