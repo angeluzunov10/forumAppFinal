3657383dc044c894607f2f7036b977c958572e5b
angeluzunov10 <angel_d_u@abv.bg>
2024-11-15 20:15:18 +0200

    Registered the new user for the admin panel. Made custom creation and custom change form for the admin panel.

--- forumApp/accounts/admin.py
+++ forumApp/accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+from forumApp.accounts.forms import CustomUserChangeForm, CustomUserCreateForm
 
 # Register your models here.
 
@@ -7,6 +10,26 @@ UserModel = get_user_model()
 
 
 @admin.register(UserModel)
-class AdminUser(admin.ModelAdmin):
-    pass
+class AppUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreateForm
+
+    list_display = ('username', 'email')
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ('wide',),
+                "fields": ('username', 'email', 'password1', 'password2'),
+            },
+        ),
+    )
+
+    fieldsets = (
+        ('Credentials', {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important Dates', {'fields': ('last_login',)}),
+    )
+
 
--- forumApp/accounts/views.py
+++ forumApp/accounts/views.py
@@ -1,9 +1,9 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from forumApp.forms import CustomUserForm
+from forumApp.accounts.forms import CustomUserCreateForm
 
 
 class UserRegisterView(CreateView):
-    form_class = CustomUserForm
+    form_class = CustomUserCreateForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('index')
